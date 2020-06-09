561243dc07b29173b7dca23202a321e87a4a8122
JasonYuan <chenghuai11yuan@sina.com>
2020-06-09 11:27:50 +0800

    修改加载预设脚本

--- assets/component/LoadPrefab.ts
+++ assets/component/LoadPrefab.ts
@@ -1,12 +1,27 @@
 
 const {ccclass, property,executeInEditMode} = cc._decorator;
 
+// // 屏蔽2.3.1版本prefab嵌套prefab的弹框问题
+// if (CC_EDITOR && !window["Editor"].isBuilder) {
+//     window["_Scene"].DetectConflict.beforeAddChild = function() {
+//         return false
+//     }
+// }
+
 @ccclass
 @executeInEditMode
 export default class LoadPrefab extends cc.Component {
 
-    @property(cc.Prefab)
-    prefab: cc.Prefab = null;
+    @property
+    private _prefab: cc.Prefab = null;
+    @property({type: cc.Prefab, visible: true, displayName: "预制体"})
+    set prefab(value: cc.Prefab) {
+        this._onPrefabChanged(this._prefab, value)
+    }
+
+    get prefab(): cc.Prefab {
+        return this._prefab
+    }
     @property(cc.Node)
     parent:cc.Node = null;
     @property
@@ -20,13 +35,29 @@ export default class LoadPrefab extends cc.Component {
     }
     //实例化预制体，设置父节点
     loadPrefab(){
+        if (!this._prefab || this._prefabNode) {
+            return
+        }
         this._prefabNode = cc.instantiate(this.prefab);
+        // cc.Object["Flags"].DontSave          // 当前节点不会被保存到prefab文件里
+        // cc.Object["Flags"].LockedInEditor    // 当前节点及子节点在编辑器里不会被点击到
+        // cc.Object["Flags"].HideInHierarchy   // 当前节点及子节点在编辑器里不显示
         this._prefabNode._objFlags |= cc.Object.Flags.DontSave;
         this._prefabNode.parent = this.parent || this.node;
         this._prefabNode.active = true;
         this._prefabNode.setPosition(cc.Vec3.ZERO);
     }
-    getPrefabComp<T extends cc.Component>(component):T{
-        return this._prefabNode.getComponent<T>(component);
+    private _onPrefabChanged(oldValue:cc.Prefab, newValue:cc.Prefab) {
+        if (this._prefabNode) {
+            this._prefabNode.destroy();
+            this._prefabNode = null;
+        }
+        this._prefab = newValue
+        if (newValue) {
+            this.loadPrefab();
+        }
+    }
+    getPrefabComp<T extends cc.Component>(component: {prototype: T}):T{
+        return this._prefabNode ? this._prefabNode.getComponent(component) : null;
     }
 }
